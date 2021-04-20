546404c526419ff5ecba8ad2cc3929aeb392a030
FranciscoJRE14 <frarodele@alum.us.es>
2021-04-20 21:03:43 +0200

    Functional requirement shout.

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
@@ -2,6 +2,7 @@
 package acme.features.anonymous.shout;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,8 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AnonymousShoutRepository  extends AbstractRepository  {
 	
-	@Query("select s from Shout s")
-	Collection<Shout> findMany();
+
+	@Query("select s from Shout s where s.moment >= ?1 order by s.moment ")
+	Collection<Shout> findLessThanAMonth(Date time);
 
 }
