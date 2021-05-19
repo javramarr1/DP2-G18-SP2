194ca7f0c9c1ca17e193e32d6317e8973d19483d
Juanmagc99 <juagarcri@alum.us.es>
2021-05-19 22:09:53 +0200

    Test provisional DashBoard 2

--- src/test/java/acme/testing/administrator/dashboard/AdministratorShowDashBoardTest.java
+++ src/test/java/acme/testing/administrator/dashboard/AdministratorShowDashBoardTest.java
@@ -1,15 +1,13 @@
 package acme.testing.administrator.dashboard;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.api.Test;
 
 import acme.testing.AcmePlannerTest;
 
 public class AdministratorShowDashBoardTest extends AcmePlannerTest{
 	
-	@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/dashboard/list.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void positiveDashBoardShow(final int recordIndex, final String value) {
+	@Test
+	public void positiveDashBoardShow() {
 		super.signIn("administrator","administrator");
 		
 		this.navigateHome();
