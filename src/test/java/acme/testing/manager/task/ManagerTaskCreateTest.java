fac63c892cf0123dc442ec35339de364e89a14ed
Juanmagc99 <juagarcri@alum.us.es>
2021-05-23 18:24:43 +0200

    Solucionado los problemas de manager test create

--- src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
@@ -10,10 +10,10 @@ import acme.testing.AcmePlannerTest;
 public class ManagerTaskCreateTest extends AcmePlannerTest {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/task/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(20)
 	public void createPositive(final int recordIndex,final String title,
-		final String start_date, final String end_date,
-		final String workload, final String description, 
+		final String description,final String start_date, 
+		final String end_date,final String workload,  
 		final String op_link, final String is_private) 
 	{
 		assert !StringHelper.isBlank(title);		
@@ -27,10 +27,10 @@ public class ManagerTaskCreateTest extends AcmePlannerTest {
 		
 		super.clickOnMenu("Manager", "Create task");
 		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("start_date", start_date);
 		super.fillInputBoxIn("end_date", end_date);
 		super.fillInputBoxIn("workload", workload);
-		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("op_link", op_link);
 		super.fillInputBoxIn("is_private", is_private);
 		
@@ -43,34 +43,28 @@ public class ManagerTaskCreateTest extends AcmePlannerTest {
 		super.checkColumnHasValue(recordIndex, 3, workload);
 		super.checkColumnHasValue(recordIndex, 4, op_link);
 		
+		
 		super.clickOnListingRecord(recordIndex);
 		
 		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
 		super.checkInputBoxHasValue("start_date", start_date);
 		super.checkInputBoxHasValue("end_date", end_date);
 		super.checkInputBoxHasValue("workload", workload);
-		super.checkInputBoxHasValue("description", description);
 		super.checkInputBoxHasValue("op_link", op_link);
 		super.checkInputBoxHasValue("is_private", is_private);
 		
 		super.signOut();
 	}
 	
-	/*@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/spam/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(20)
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
 	public void createNegative(final int recordIndex,final String title,
 		final String start_date, final String end_date,
 		final String workload, final String description,
 		final String op_link, final String is_private) 
-	{
-		assert !StringHelper.isBlank(title);		
-		assert !StringHelper.isBlank(start_date);	
-		assert !StringHelper.isBlank(end_date);	
-		assert !StringHelper.isBlank(workload);	
-		assert !StringHelper.isBlank(description);	
-		assert !StringHelper.isBlank(op_link);	
-		assert !StringHelper.isBlank(is_private);	
+	{	
 		
 		super.signIn("manager", "manager");
 		
@@ -82,9 +76,10 @@ public class ManagerTaskCreateTest extends AcmePlannerTest {
 		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("op_link", op_link);
 		super.fillInputBoxIn("is_private", is_private);
+		super.clickOnSubmitButton("Create Task");
 		
-		super.clickOnSubmitButton("Create");
+		super.checkErrorsExist();
 		
 		super.signOut();
-	}*/
+	}
 }
