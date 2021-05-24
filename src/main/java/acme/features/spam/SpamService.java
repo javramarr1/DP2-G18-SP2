d3466b801a706d1d6d2f6619898ef19e4bc367a0
javramarr1 <jramosarrondo@gmail.com>
2021-05-25 00:33:49 +0200

    spam filter update

--- src/main/java/acme/features/spam/SpamService.java
+++ src/main/java/acme/features/spam/SpamService.java
@@ -1,6 +1,7 @@
 package acme.features.spam;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,19 +16,25 @@ public class SpamService {
 		
 		final List<String> spamWords = this.spamRepository.findAllSpamWords();
 		final String cutText = text.replace(" ", "").replace("\n", "n").toLowerCase().trim();
-		final Integer numberOfLetters = cutText.length();
+		final int words = this.numberOfWords(text);
 		Integer counter = 0; 
 		for(int i = 0; i<spamWords.size();i++) {
 			final String word = spamWords.get(i).replace(" ", "").toLowerCase().trim();
 			boolean check = cutText.contains(word);
 			while(check) {
-				counter+=word.length();
+				counter++;
 				check = cutText.substring(cutText.indexOf(word)+word.length()).contains(word);
 			}
 		}
 		
 		final Double t = this.spamRepository.findThreshold();
-		return (counter*100.0/numberOfLetters)<=t;	
+		return (counter*100.0/words)<=t;	
+	}
+
+	
+	private int numberOfWords(final String text) {
+		final StringTokenizer st = new StringTokenizer(text);
+		return st.countTokens();
 	}
 
 }
